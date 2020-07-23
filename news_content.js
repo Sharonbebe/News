6e486de8a2b9e15ea19b467965a4c31400c00f11
SharonLaio <46222642+SharonLaio@users.noreply.github.com>
2020-07-23 11:06:51 +0800

    add obj

---
+++ news_content.js
@@ -0,0 +1,30 @@
+var news_list =
+{
+
+    "國際": {
+        1:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+        2:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+        3:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        }
+    
+    }
+}
