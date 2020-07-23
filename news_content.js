de42160c759916855daf840d3576f6ba66c514e9
jiawenliao <jiawen4516@cht.com.tw>
2020-07-23 18:15:59 +0800

    update

--- news_content.js
+++ news_content.js
@@ -9,6 +9,274 @@ var news_list =
             "subkind": "國際大事"
         },
         2:{
+            "title": "北京仍呼籲合作，但已加強抗衡美國的決心",
+            "img": "images/yahoo/new_p_18.jpg",
+            "kind": "國際",
+            "subkind": "紐約時報"
+        },
+        3:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+        4:{
+           "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "紐約時報"             
+        },
+        5:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+        6:{
+           "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"             
+        }
+            
+    },
+    "政治": {
+        1:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+        2:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+        3:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+        4:{
+           "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"             
+        },
+        5:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+        6:{
+           "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"             
+        }
+    "地方": {
+        1:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+        2:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+        3:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+    "財經": {
+        1:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+        2:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+        3:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+    "體育": {
+        1:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+        2:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+        3:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+    "科技": {
+        1:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+        2:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+        3:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+    "娛樂": {
+        1:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+        2:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+        3:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+    "健康": {
+        1:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+        2:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+        3:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+    "生活": {
+        1:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+        2:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+        3:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+    "天氣": {
+        1:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+        2:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+        3:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+    "影音": {
+        1:{
+            "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
+            "img": "images/yahoo/new_p_1.jpg",
+            "kind": "國際",
+            "subkind": "國際大事"
+        },
+        2:{
             "title": "香港發起「反惡歌法」大遊行，港警舉旗射煙霧彈驅離",
             "img": "images/yahoo/new_p_1.jpg",
             "kind": "國際",
@@ -26,5 +294,4 @@ var news_list =
             "subkind": "國際大事"
         }
     
-    }
 }
